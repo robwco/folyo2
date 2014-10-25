ca7989438dcfde1e22be77cd13a84577fbffb929
Robert Williams <robwilliamsgd@gmail.com>
2014-10-25 09:45:18 -0700

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141024191452) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "leads", force: true do |t|
     t.string   "photo"
     t.string   "title"
