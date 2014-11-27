6949d00dad5fb63756bd50de7cbb1e5296a8a248
Robert Williams <robwilliamsgd@gmail.com>
2014-11-26 16:22:41 -0800

    update home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     @leads = Lead.all
     @exclusives = Exclusive.all
+    @sent_this_week = @leads.this_week.map{ |lead| lead.budget.to_i }.reduce(:+) + @exclusives.this_week.map{ |exclusive| exclusive.budget.to_i }.reduce(:+)
   end
 end
--- app/models/exclusive.rb
+++ app/models/exclusive.rb
@@ -2,6 +2,7 @@ class Exclusive < ActiveRecord::Base
 	validates_presence_of :title, :url, :name, :email
 	scope :most_recent, -> { order("created_at desc").limit(50) }
 	scope :today, -> { where(:created_at => ((Time.now - 20.hours)..Time.now)) }
+	scope :this_week, -> { where(:created_at => ((Time.now - 7.days)..Time.now)) }
 	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :pic, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
