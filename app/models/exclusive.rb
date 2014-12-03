84343417a8bb4bfdbec97ee444f8ebe97bacdd83
Robert Williams <robwilliamsgd@gmail.com>
2014-12-03 10:29:20 -0800

    fix exclusive form remove url

--- app/models/exclusive.rb
+++ app/models/exclusive.rb
@@ -1,5 +1,5 @@
 class Exclusive < ActiveRecord::Base
-	validates_presence_of :title, :url, :name, :email
+	validates_presence_of :title, :name, :email, :description, :category
 	scope :most_recent, -> { order("created_at desc").limit(50) }
 	scope :today, -> { where(:created_at => ((Time.now - 20.hours)..Time.now)) }
 	scope :this_week, -> { where(:created_at => ((Time.now - 7.days)..Time.now)) }
