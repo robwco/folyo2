a30b9c9438346cc0318e2d436cdd1937db37aa28
Robert Williams <robwilliamsgd@gmail.com>
2014-12-03 15:33:27 -0800

    remove onboard route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   get "/successful_order" => "pages#successful_order"
   get "/people" => "pages#people_product_test"
   get "/successful_featured" => "exclusives#success"
-  get "/onboard" => "leads#onboard"
   get '*path' => redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
