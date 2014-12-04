0c991e3f5234585f3e3b06bbe19e5a5382c65b2d
Robert Williams <robwilliamsgd@gmail.com>
2014-12-03 16:46:53 -0800

    design the onboard page

--- app/models/exclusive.rb
+++ app/models/exclusive.rb
@@ -4,6 +4,6 @@ class Exclusive < ActiveRecord::Base
 	scope :today, -> { where(:created_at => ((Time.now - 20.hours)..Time.now)) }
 	scope :this_week, -> { where(:created_at => ((Time.now - 7.days)..Time.now)) }
 	scope :recent_onboard, -> { where(:created_at => ((Time.now - 3.days)..Time.now)) }
-	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :pic, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
