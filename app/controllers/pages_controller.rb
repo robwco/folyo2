162eac4f8b3641ce97a15badb2360b4228e20b98
Robert Williams <robwilliamsgd@gmail.com>
2014-12-03 22:49:18 -0800

    change sent this week total

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def home
     @leads = Lead.all
     @exclusives = Exclusive.all
-    @sent_this_week = @leads.this_week.map{ |lead| lead.budget.to_i }.reduce(1000, :+) + @exclusives.this_week.map{ |exclusive| exclusive.budget.to_i }.reduce(0, :+)
+    @number_of_leads_this_week = Lead.this_week.distinct.count
+    @sent_this_week = @leads.this_week.map{ |lead| lead.budget.to_i }.reduce((@number_of_leads_this_week * 1000), :+) + @exclusives.this_week.map{ |exclusive| exclusive.budget.to_i }.reduce(0, :+)
   end
   def about
 	end
