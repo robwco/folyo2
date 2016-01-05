69c5c908bd10707f36cc6bf4537bc7dd025069d8
Robert Williams <robwilliamsgd@gmail.com>
2016-01-05 10:35:28 -0800

    add more states

--- app/models/favorite_lead.rb
+++ app/models/favorite_lead.rb
@@ -4,17 +4,4 @@ class FavoriteLead < ActiveRecord::Base
 
   has_many :state, through: :user
 
-  aasm column: 'state' do
-		state :first_contact, initial: true
-		state :follow_up
-		state :touch_base
-  end
-
-  event :add_to_follow_up do
-  	transitions :to => :follow_up, from [:any]
-  end
-
-  event :add_to_touch_base do
-  	transitions :to => :touch_base, from [:any]
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206191654) do
+ActiveRecord::Schema.define(version: 20160104230201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20151206191654) do
 
   add_index "categories_users", ["category_id"], name: "index_categories_users_on_category_id", using: :btree
   add_index "categories_users", ["user_id"], name: "index_categories_users_on_user_id", using: :btree
+
+  create_table "clients", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
@@ -117,6 +124,7 @@ ActiveRecord::Schema.define(version: 20151206191654) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "state"
   end
 
   create_table "imported_plans", force: true do |t|
