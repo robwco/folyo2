625341ce155d401cd29759b4598bc0b75ea2acab
Robert Williams <robwilliamsgd@gmail.com>
2016-01-18 21:02:49 -0800

    change weekly email to tuesday

--- app/services/weekly_leads_job.rb
+++ app/services/weekly_leads_job.rb
@@ -1,9 +1,9 @@
 class WeeklyLeadsJob
-	def perform
-		return unless Time.now.monday? && user.favorite_leads.present?
+  def perform
+    return unless Time.now.tuesday?
 
-		User.active.each do |user|
-			WorkerMailer.delay.weekly_leads(user)
-		end
-	end
+    User.active.each do |user|
+      WorkerMailer.delay.weekly_leads(user) if user.favorite_leads.present?
+    end
+  end
 end
