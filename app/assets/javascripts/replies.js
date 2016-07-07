5afafe9ebf7ec22f99415fe54a2ec4355364fd5a
Jason Forrest <jason.forrest@rigbooks.com>
2016-07-07 10:55:14 -0700

    Add character counters on replies

---
+++ app/assets/javascripts/replies.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".textarea-wrap textarea[data-max-characters]").each(function() {
+		var counter = $(this).parents(".textarea-wrap").find(".counter");
+		$(this).keyup(function() {
+			var remaining = $(this).data("max-characters") - $(this).val().length;
+			counter.text("" + remaining + " characters left");
+		});
+	});
+});
