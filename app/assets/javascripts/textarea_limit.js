2b3c79b266e7b4c6419aa0f2251617703292a576
Jason Forrest <jason.forrest@rigbooks.com>
2016-08-12 15:55:54 -0700

    Add character counters

---
+++ app/assets/javascripts/textarea_limit.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	function countChars(text, counter) {
+	  var max = parseInt(text.data('max-characters'),10);
+	  var chars = text.val().length;
+
+	  counter.text(max - chars);
+
+	  if ((max - chars) < 0) {
+		counter.css('color', '#d9666a');
+	  } else {
+		counter.css('color', '#000');
+	  }
+	}
+
+	$("[data-max-characters]").each(function(){
+	  var counterElem = $("<div style='text-align:right;'></div>");
+	  counterElem.width($(this).width()).css('padding', $(this).css('padding')).css('margin', $(this).css('margin'));
+	  counterElem.css('padding-top', '0px').css('padding-bottom','0px');
+
+	  $(this).after(counterElem);
+	  countChars($(this), counterElem);
+
+	  $(this).keyup(function() {
+		countChars($(this), counterElem);
+	  });
+	});
+});
