afcc85ae263048e817c8cc1e5236d6725261eec6
Jason Forrest <jason.forrest@rigbooks.com>
2016-08-31 11:11:11 -0700

    Add support for easy image upload and preview

---
+++ app/assets/javascripts/preview_photo_uploads.js
@@ -0,0 +1,21 @@
+$(function() {
+	$("[data-preview-image-target]").each(function(){
+		var target = $("#" + $(this).data("preview-image-target"));
+		if (target) {
+		  $(this).on('change', function(event) {
+			var files = event.target.files;
+			var image = files[0]
+			// here's the file size
+			console.log(image.size);
+			var reader = new FileReader();
+			reader.onload = function(file) {
+			  target.attr('src', file.target.result);
+			  target.show();
+			}
+			reader.readAsDataURL(image);
+			console.log(files);
+		  });
+
+		}
+	});
+});
