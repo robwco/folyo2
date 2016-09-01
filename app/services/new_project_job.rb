ac6f63da539071ee071777b379773aef2e167dbf
Jason Forrest <jason.forrest@rigbooks.com>
2016-09-01 11:25:52 -0700

    Fix duplicate email for new projects if freelancer matches more than one skill with project

--- app/services/new_project_job.rb
+++ app/services/new_project_job.rb
@@ -2,7 +2,7 @@ class NewProjectJob < Struct.new(:project_id)
   def perform
     project = Project.find project_id
 
-    User.joins(:categories).active.freelancers.where(categories: { id: project.category_ids }).each do |user|
+    User.joins(:categories).active.freelancers.where(categories: { id: project.category_ids }).distinct.each do |user|
       ProjectMailer.delay.new_project(user, project)
     end
   end
