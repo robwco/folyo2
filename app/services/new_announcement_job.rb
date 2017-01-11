f854591d80aa3d432e521b37e5952a0e15b6887c
Robert Williams <robwilliamsgd@gmail.com>
2017-01-11 13:47:42 -0800

    test changes

--- app/services/new_announcement_job.rb
+++ app/services/new_announcement_job.rb
@@ -2,7 +2,7 @@ class NewAnnouncementJob < Struct.new(:announcement_id)
   def perform
     announcement = Announcement.find announcement_id
 
-    User.where(:email => "robwilliamsgd@gmail.com").each do |user|
+    User.each do |user|
       AnnouncementMailer.delay.new_announcement(user, announcement)
     end
   end
