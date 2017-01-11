e8ed8351439226791d217cc10f8fa20a4436b3de
Robert Williams <robwilliamsgd@gmail.com>
2017-01-11 14:38:19 -0800

    fix mailer

--- app/services/new_announcement_job.rb
+++ app/services/new_announcement_job.rb
@@ -2,7 +2,7 @@ class NewAnnouncementJob < Struct.new(:announcement_id)
   def perform
     announcement = Announcement.find announcement_id
 
-    User.each do |user|
+    User.all.each do |user|
       AnnouncementMailer.delay.new_announcement(user, announcement)
     end
   end
