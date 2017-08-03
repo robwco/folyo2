58a143fe2c19fe65529120972842bb8d3dfda4c6
Robert Williams <robwilliamsgd@gmail.com>
2017-08-02 18:46:45 -0700

    up to date?

--- config/routes.rb
+++ config/routes.rb
@@ -98,7 +98,7 @@ Rails.application.routes.draw do
   get "/guides/how_to_write_a_good_job_description", to: redirect("https://clientgiant.us/how-to-write-a-job-ad-and-attract-the-right-people-to-your-project-9b8dd921e5e8#.vjyjulim6", status: 301)
 
   # Sign in
-  root "pages#simple"
+  root "projects#home"
   get "/admins/welcome" => "admins#welcome", as: :admin_root
   
   devise_scope :user do 
